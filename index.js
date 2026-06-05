a8ac58c3be72bae99d29c9093f05ac61b312d6e0
saadmannur <saadmannur99@gmail.com>
2026-06-05 17:06:48 +0600

    added users route and users data

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5002;
+
+app.use(cors());
+app.use(express.json())
+
+
+const uri = "mongodb+srv://simpleCrudUsers:5sJly2bEntwUatkV@cluster0.nbebzri.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+
+const run = async () => {
+    try {
+        await client.connect();
+
+
+        const db = client.db('simpleCrud');
+        const userCollection = db.collection('users');
+
+        app.get('/users', async (req, res) => {
+            const cursor = userCollection.find();
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
+
+        await client.db('admin').command({ ping: 1 })
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    }
+    finally{
+        // await client.close();
+    }
+}
+run().catch(console.dir)
+
+
+//mongodb+srv://simpleCrudUsers:5sJly2bEntwUatkV@cluster0.nbebzri.mongodb.net/?appName=Cluster0
+
+app.get('/', (req, res) => {
+    res.send("simple CRUD server is serving")
+})
+
+app.listen(port, () => {
+    console.log(`simple CRUD server is running on port ${port}`)
+})
